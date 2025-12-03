c68aaab744c02255262165ac4275f8521014b4f0
Adam Kilpatrick <adamckilpatrick@gmail.com>
2025-12-03 13:56:36 -0600

    disable SPA mode

--- .quartz/quartz.config.ts
+++ .quartz/quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Trail Notes",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: null,

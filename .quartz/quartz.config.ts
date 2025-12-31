fe367503f8d1187bcfbc6bb2b71bfa54bd6be2a8
Adam Kilpatrick <adamckilpatrick@gmail.com>
2025-12-30 19:34:50 -0600

    Maybe fix modified date issue

--- .quartz/quartz.config.ts
+++ .quartz/quartz.config.ts
@@ -57,7 +57,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem", "git"],
+        priority: ["git"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
